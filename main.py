58328e81e249b22d61615191ccd9d3a042ddc9d2
beccakosey22 <820512@mystma.org>
2021-09-29 09:33:09 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called isEven() that returns true/false if a given variable n is even or odd
+def isEven(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
